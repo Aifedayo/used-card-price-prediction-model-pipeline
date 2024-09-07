825f1bd276ceea4a24604ad86724ba5b3511ff97
Aifedayo <akeemifedayolag@gmail.com>
2024-09-07 19:55:44 +0100

    Refactor data transformation script

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -9,7 +9,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, LabelEncoder
 
 from src.exception import CustomException
 from src.logger import logging
@@ -26,18 +26,35 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
 
-    def rename_columns_with_underscore(self):
-        pass
-
-    def remove_outlier(self):
-        pass
-
     def get_data_transformer_object(self):
         """
-        This function is responsible for data transformation
+        This function is responsible for creating a pipeline
+        that will transform the data.
         """
         try:
-            pass
+            # Pipeline for scaling numerical features
+            numeric_cols = ['Mileage']
+            numeric_pipeline = Pipeline(
+                steps=[
+                    ("scaler", StandardScaler())
+                ]
+            )
+
+            # Pipeline for encoding categorical features
+            leather_interior = ['Leather_interior']
+            leather_pipeline = Pipeline(
+                steps=[
+                    ('onehot', OneHotEncoder(drop='first'))
+                ]
+            )
+
+            # Combine the pipelines into a single ColumnTransformer
+            preprocessor = ColumnTransformer([
+                ("numeric_pipeline", numeric_pipeline, numeric_cols),
+                ("leather_pipeline", leather_pipeline, leather_interior)
+            ])
+
+            return preprocessor
 
         except Exception as e:
             raise CustomException(e, sys)
