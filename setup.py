2a7fade49e2124b70dfd052976a37ba18c723d25
Aifedayo <akeemifedayolag@gmail.com>
2024-09-07 12:02:17 +0100

    Complete the setup, install as a package

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ def get_requirements(filepath:str)->List[str]:
     This func will read the requirements.txt file
     and append it to a list
     """
-    with open('requirements.txt') as file:
+    with open(filepath, 'r') as file:
         requirements = file.readlines()
-        requirements = [req.replace('/n', '') for req in requirements]
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
+    print(requirements, '<<<<<<<<<<<<<<<<<<<')
     return requirements
 
 setup(
@@ -22,6 +23,6 @@ setup(
     version='0.0.1',
     author='Akeem I. Lagundoye',
     author_email='akeemifedayolag@gmail.com',
-    requires=get_requirements('requirements.txt'),
     packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
 )
